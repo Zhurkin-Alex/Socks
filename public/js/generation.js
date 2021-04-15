a5e77404a20e6697a437e04fb7d2a9ee2b0c5efd
Ilia <ershovilya97@gmail.com>
2021-04-15 11:24:29 +0300

    upd123

--- public/js/generation.js
+++ public/js/generation.js
@@ -2,15 +2,14 @@ const form = document.querySelector('#selectcolors');
 if (form !== null) {
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const { tagselect } = event.target
-    const { colorselect } = event.target
-    console.log(tagselect.value, colorselect.value)
+    const { colorselect, patternselect, imgselect } = event.target;
+    console.log(colorselect.value)
     const response = await fetch('/mysocks', {
       method: 'POST',
       headers: {
         "Content-Type": "Application/json",
       },
-      body: JSON.stringify({ color: colorselect.value, pattern: tagselect.value })
+      body: JSON.stringify({ color: colorselect.value, pattern: patternselect.value, img: imgselect.value })
     })
     const result = await response.text();
     console.log(result);
