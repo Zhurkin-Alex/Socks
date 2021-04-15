20b49984fb7c7d09d5ef171f1d75e2673b16dfc9
Zhurkin_Alex <lexus1812141@gmail.com>
2021-04-15 15:33:36 +0300

    rel sliv

--- models/socks.js
+++ models/socks.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const socksSchema = new mongoose.Schema({
   color: { type: String, required: true },
   pattern:  { type: String, required: true },
-  img:  { type: String, required: true }
+  img:  { type: String, required: true },
+  count:{type:Number, default:0},
 });
 
 module.exports = mongoose.model("socks", socksSchema);
